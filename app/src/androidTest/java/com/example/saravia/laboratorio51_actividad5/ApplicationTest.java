8c184e585e20e80b0aad63531a8b1452272cd41b
saravia <tecla253@gmail.com>
2015-09-27 16:39:39 -0500

    Initial commit

---
+++ app/src/androidTest/java/com/example/saravia/laboratorio51_actividad5/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.saravia.laboratorio51_actividad5;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
